e004a2c25b3c9146679ce77b2092a946285d215b
André Missaglia <andre.missaglia@gmail.com>
2016-06-06 11:23:35 -0300

    Adiciona FrustumCulling

---
+++ src/main/java/br/usp/icmc/vicg/projeto/FrustumCulling.java
@@ -0,0 +1,120 @@
+package br.usp.icmc.vicg.projeto;
+
+import br.usp.icmc.vicg.gl.matrix.Matrix4;
+
+public class FrustumCulling {
+
+    float frustum[][];
+
+    public FrustumCulling() {
+        frustum = new float[6][4];
+    }
+
+    public void extractFrustum(Matrix4 projection, Matrix4 view) {
+        float t;
+        Matrix4 clipm;
+        float[] clip;
+        clipm = new Matrix4(projection);
+        clipm.multiply(view);
+        clip = clipm.getVals();
+        /* Extract the numbers for the RIGHT plane */
+        frustum[0][0] = clip[3] - clip[0];
+        frustum[0][1] = clip[7] - clip[4];
+        frustum[0][2] = clip[11] - clip[8];
+        frustum[0][3] = clip[15] - clip[12];
+
+        /* Normalize the result */
+        t = (float) Math.sqrt(frustum[0][0] * frustum[0][0] + frustum[0][1] * frustum[0][1] + frustum[0][2] * frustum[0][2]);
+        frustum[0][0] /= t;
+        frustum[0][1] /= t;
+        frustum[0][2] /= t;
+        frustum[0][3] /= t;
+
+        /* Extract the numbers for the LEFT plane */
+        frustum[1][0] = clip[3] + clip[0];
+        frustum[1][1] = clip[7] + clip[4];
+        frustum[1][2] = clip[11] + clip[8];
+        frustum[1][3] = clip[15] + clip[12];
+
+        /* Normalize the result */
+        t = (float) Math.sqrt(frustum[1][0] * frustum[1][0] + frustum[1][1] * frustum[1][1] + frustum[1][2] * frustum[1][2]);
+        frustum[1][0] /= t;
+        frustum[1][1] /= t;
+        frustum[1][2] /= t;
+        frustum[1][3] /= t;
+
+        /* Extract the BOTTOM plane */
+        frustum[2][0] = clip[3] + clip[1];
+        frustum[2][1] = clip[7] + clip[5];
+        frustum[2][2] = clip[11] + clip[9];
+        frustum[2][3] = clip[15] + clip[13];
+
+        /* Normalize the result */
+        t = (float) Math.sqrt(frustum[2][0] * frustum[2][0] + frustum[2][1] * frustum[2][1] + frustum[2][2] * frustum[2][2]);
+        frustum[2][0] /= t;
+        frustum[2][1] /= t;
+        frustum[2][2] /= t;
+        frustum[2][3] /= t;
+
+        /* Extract the TOP plane */
+        frustum[3][0] = clip[3] - clip[1];
+        frustum[3][1] = clip[7] - clip[5];
+        frustum[3][2] = clip[11] - clip[9];
+        frustum[3][3] = clip[15] - clip[13];
+
+        /* Normalize the result */
+        t = (float) Math.sqrt(frustum[3][0] * frustum[3][0] + frustum[3][1] * frustum[3][1] + frustum[3][2] * frustum[3][2]);
+        frustum[3][0] /= t;
+        frustum[3][1] /= t;
+        frustum[3][2] /= t;
+        frustum[3][3] /= t;
+
+        /* Extract the FAR plane */
+        frustum[4][0] = clip[3] - clip[2];
+        frustum[4][1] = clip[7] - clip[6];
+        frustum[4][2] = clip[11] - clip[10];
+        frustum[4][3] = clip[15] - clip[14];
+
+        /* Normalize the result */
+        t = (float) Math.sqrt(frustum[4][0] * frustum[4][0] + frustum[4][1] * frustum[4][1] + frustum[4][2] * frustum[4][2]);
+        frustum[4][0] /= t;
+        frustum[4][1] /= t;
+        frustum[4][2] /= t;
+        frustum[4][3] /= t;
+
+        /* Extract the NEAR plane */
+        frustum[5][0] = clip[3] + clip[2];
+        frustum[5][1] = clip[7] + clip[6];
+        frustum[5][2] = clip[11] + clip[10];
+        frustum[5][3] = clip[15] + clip[14];
+
+        /* Normalize the result */
+        t = (float) Math.sqrt(frustum[5][0] * frustum[5][0] + frustum[5][1] * frustum[5][1] + frustum[5][2] * frustum[5][2]);
+        frustum[5][0] /= t;
+        frustum[5][1] /= t;
+        frustum[5][2] /= t;
+        frustum[5][3] /= t;
+    }
+
+    boolean isPointInFrustum(float x, float y, float z) {
+        int p;
+
+        for (p = 0; p < 6; p++) {
+            if (frustum[p][0] * x + frustum[p][1] * y + frustum[p][2] * z + frustum[p][3] <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    boolean isSphereInFrustum(float x, float y, float z, float radius) {
+        int p;
+
+        for (p = 0; p < 6; p++) {
+            if (frustum[p][0] * x + frustum[p][1] * y + frustum[p][2] * z + frustum[p][3] <= -radius) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
